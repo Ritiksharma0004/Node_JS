d1e75ded3f011c2dfbef914b4f586ef6e6097510
RitikSharma0004 <ritiksharma9057879116@gmail.com>
2024-11-10 19:25:06 +0530

    Authentication enabled auth.js

--- my_server.js
+++ my_server.js
@@ -3,14 +3,27 @@ const app = express();
 app.use(express.json());
 const db = require("./db.js");
 require("dotenv").config();
+const passport = require("./auth.js")
+
+
+
+
+
 
 // Middleware Function
 const logRequest = (req,res,next) => {
   console.log(`[${new Date().toLocaleString()}] Request made to: ${req.originalUrl}`)
   next();
 }
-
 app.use(logRequest);
+
+
+
+//Authentication part
+app.use(passport.initialize());
+
+const localAuthMiddleware = passport.authenticate('local', {session: false});
+
 app.get("/", (req, res) => {
   res.send("Connect to database");
 });
@@ -23,7 +36,7 @@ const menuRoutes = require("./routes/menuRoutes.js");
 const PORT = process.env.PORT || 3000;
 
 // Use the routers
-app.use("/person", personRoutes);
+app.use("/person",localAuthMiddleware,  personRoutes);
 app.use("/menuItem", menuRoutes);
 
 
